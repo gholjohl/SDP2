0341bec039961811cf2c67c31b3a09ce0a3b2cd1
elaiaep <gholjohl3@gmail.com>
2024-10-30 00:14:19 +0500

    Добавлен главный класс для запуска приложения

---
+++ src/main/CalculatorApp.java
@@ -0,0 +1,13 @@
+package main;
+
+import model.CalculatorModel;
+import view.CalculatorView;
+import controller.CalculatorController;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        CalculatorModel model = new CalculatorModel();
+        CalculatorView view = new CalculatorView();
+        CalculatorController controller = new CalculatorController(model, view);
+    }
+}
